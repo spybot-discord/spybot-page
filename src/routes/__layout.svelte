dd58c736fe365e7fe62f2ae25ace538b9c66bc56
AngelNext <angelgjs.h4cker@gmail.com>
2022-05-17 00:03:25 +0200

    Made navbar sticky

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,10 +9,11 @@
 	<title>Spy Bot</title>
 </svelte:head>
 
-<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+<nav class="dark:text-white fixed w-full top-0 bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
 	<div class="container flex flex-wrap justify-between items-center mx-auto">
-		<a href={base}>
-			<img src="{base}/spy-bot.png" class="mr-3 h-6 sm:h-16 rounded-full" alt="Spy Bot" />
+		<a href="{base}">
+			<img src="{base}/spy-bot.png" class="mr-3 h-6 sm:h-16 rounded-full inline-block" alt="Spy Bot" />
+			<p class="inline-block text-2xl font-title">Spy Bot</p>
 		</a>
 		<button
 			data-collapse-toggle="mobile-menu"
@@ -22,27 +23,17 @@
 			aria-expanded="false"
 		>
 			<span class="sr-only">Open main menu</span>
-			<svg
-				class="w-6 h-6"
-				fill="currentColor"
-				viewBox="0 0 20 20"
-				xmlns="http://www.w3.org/2000/svg"
+			<svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
 				><path
 					fill-rule="evenodd"
 					d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-					clip-rule="evenodd"
-				/></svg
+					clip-rule="evenodd"></path></svg
 			>
-			<svg
-				class="hidden w-6 h-6"
-				fill="currentColor"
-				viewBox="0 0 20 20"
-				xmlns="http://www.w3.org/2000/svg"
+			<svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
 				><path
 					fill-rule="evenodd"
 					d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-					clip-rule="evenodd"
-				/></svg
+					clip-rule="evenodd"></path></svg
 			>
 		</button>
 		<div class="hidden w-full md:block md:w-auto" id="mobile-menu">
@@ -73,4 +64,6 @@
 	</div>
 </nav>
 
-<slot />
+<div class="mt-28">
+	<slot />
+</div>
